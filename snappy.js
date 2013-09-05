c779657291764ad9a8264d9beabe4d5c1839d71e
Carson Evans <cevans@fiercemarkets.com>
2013-09-04 23:22:02 -0400

    added snappy object processing and hipchat message sends

--- snappy.js
+++ snappy.js
@@ -3,42 +3,67 @@ var querystring = require('querystring');
 var settings = require('./configuration.js');
 var hipchat = require('./hipchat.js');
 
-function contact_created(data) {
-    console.log('contact_created: ' + querystring.stringify(data));
-    hipchat.send('html', '<b>Contact Created</b>');
-    hipchat.send('text', '/code ' + querystring.stringify(data));
-}
+// function contact_created(data) {
+//     console.log('contact_created: ' + querystring.stringify(data));
+//     hipchat.send('html', '<b>Contact Created</b>');
+//     hipchat.send('text', '/code ' + querystring.stringify(data));
+// }
 
 function message_incoming(data) {
-    console.log('message_incoming: ' + querystring.stringify(data));
+    var message = 'New incoming message on ticket #' + data.ticket_id + ' by ' + data.creator.value + ':<br />'
+    message += '<em>' + data.content + '</em><br />';
+    hipchat.send('html', message);
 }
 
 function message_outgoing(data) {
-    console.log('message_outgoing: ' + querystring.stringify(data));
+    var message = 'New outgoing message on ticket #' + data.ticket_id + ' by ' + data.creator.value + ':<br />'
+    message += '<em>' + data.content + '</em><br />';
+    hipchat.send('html', message);
 }
 
 function message_private(data) {
-    console.log('message_private: ' + querystring.stringify(data));
+    var message = 'New private message on ticket #' + data.ticket_id + ' by ' + data.creator.value + ':<br />'
+    message += '<em>' + data.content + '</em><br />';
+    hipchat.send('html', message);
 }
 
 function tags_changed(data) {
-    console.log('tags_changed: ' + querystring.stringify(data));
+    var message = 'Ticket tags updated:<br />'
+    message += 'Subject: ' + data.default_subject + '<br />';
+    message += 'From: ' + data.opener.value + '<br />';
+    if (data.added) {
+        message += 'Added: ' + data.added + '<br />';
+    }
+    if (data.added) {
+        message += 'Removed: ' + data.removed + '<br />';
+    }
+
+    hipchat.send('html', message);
 }
 
 function ticket_created(data) {
-    console.log('ticket_created: ' + querystring.stringify(data));
+    var message = 'Ticket created:<br />'
+    message += 'Subject: ' + data.default_subject + '<br />';
+    message += 'From: ' + data.opener.value;
+    hipchat.send('html', message);
 }
 
 function ticket_replied(data) {
-    console.log('ticket_replied: ' + querystring.stringify(data));
+    var message = 'Ticket marked replied:<br />'
+    message += 'Subject: ' + data.default_subject + '<br />';
+    message += 'From: ' + data.opener.value;
+    hipchat.send('html', message);
 }
 
 function ticket_waiting(data) {
-    console.log('ticket_waiting: ' + querystring.stringify(data));
+    var message = 'Ticket marked waiting:<br />'
+    message += 'Subject: ' + data.default_subject + '<br />';
+    message += 'From: ' + data.opener.value;
+    hipchat.send('html', message);
 }
 
 
-exports.contact_created = contact_created;
+//exports.contact_created = contact_created;
 exports.message_incoming = message_incoming;
 exports.message_outgoing = message_outgoing;
 exports.message_private = message_private;
